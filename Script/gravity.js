2d5be1652c614ad2a10b8deae71759247646f07f
aenen <yaroslav.golyuk@yandex.ua>
2017-10-16 21:45:39 +0300

    gravity.js script improved

--- Script/gravity.js
+++ Script/gravity.js
@@ -1,8 +1,8 @@
 (function ($) {
 	var settings = {
 		fps: 60,
-		speed: 4,
-		gravity: 0.3
+		speed: 5,
+		gravity: 0.5
 	};
 
 	function onClick(e) {
@@ -12,25 +12,27 @@
 
 		var speed = settings.speed;
 		var pos = $(this).offset();
-		$(this).appendTo($("body")).css("position", "relative").offset(pos);
+		$(this).appendTo($("body")).css("position", "absolute").offset(pos);
 		$(this).addClass("gravity-fall");
 		var self = this;
 		var timerFall = setInterval(function () {
 			var bodyHeight = $("body").outerHeight();
-			var elBottom = $(self).offset().top + $(self).outerHeight();
+			var elBottom = $(self).position().top + $(self).outerHeight();
 
-			if (speed + settings.gravity < 30) {
+			if (speed + settings.gravity < 40) {
 				speed += settings.gravity;
 			}
 			if (elBottom + speed > bodyHeight) {
+				var newtop = $(self).offset().top + (bodyHeight - elBottom);
+				$(self).offset({
+					top: newtop
+				});
 				clearInterval(timerFall);
 				return true;
 			}
-
 			var newtop = $(self).offset().top + speed;
 			$(self).offset({
-				top: newtop,
-				left: pos.left
+				top: newtop
 			});
 		}, 1000 / settings.fps);
 	}
